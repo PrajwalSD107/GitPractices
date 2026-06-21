6153e7f95976ef8fcdee04c46874a83998ef4f98
PrajwalSD107 <prajwalkalaskar107@gmail.com>
2026-06-21 16:53:33 +0530

    updates auth

--- src/main/java/org/example/auth.java
+++ src/main/java/org/example/auth.java
@@ -3,5 +3,11 @@ package org.example;
 public class auth {
     public static void main(String[] args){
         System.out.println("Backend developer");
+
+        int arr[] = {1,2,3,4,5,6,7,8,9};
+
+        for(int i=1; i<=10; i++){}
+        System.out.println(i);
+
     }
 }
