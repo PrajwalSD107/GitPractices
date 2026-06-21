fd9bc041a6ca140fb1b632c2b13feb7d492f66dc
PrajwalSD107 <prajwalkalaskar107@gmail.com>
2026-06-21 15:27:00 +0530

    user and db file are modified as per the bussinecss requement

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,7 +1,11 @@
 package org.example;
 
 public class User {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello Prajwal!");
     }
+
+    int i = 01;
+    int j = 20;
 }
