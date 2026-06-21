f73f1fc034c9c06d8714e5546f5467b4d579e532
PrajwalSD107 <prajwalkalaskar107@gmail.com>
2026-06-21 16:51:51 +0530

    Add main method to auth class

--- src/main/java/org/example/auth.java
+++ src/main/java/org/example/auth.java
@@ -3,5 +3,24 @@ package org.example;
 public class auth {
     public static void main(String[] args){
         System.out.println("Backend developer");
+
+
+
+
+
+
+
+
+
+        int i = 75000;
+
+
+
+
+
+
+
+
+        
     }
 }
