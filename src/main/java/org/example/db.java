7a26697561cd36c71405863ad26c874dc3dfb1c6
PrajwalSD107 <prajwalkalaskar107@gmail.com>
2026-06-21 16:42:52 +0530

    Add additional print statements for names

--- src/main/java/org/example/db.java
+++ src/main/java/org/example/db.java
@@ -9,5 +9,15 @@ public class db {
 
         int x = 900000;
 
+
+        System.out.println("Prajwal Kalaskar");
+
+        System.out.println("savita kalaskar");
+
+        System.out.println("Pallavi Kalaskar");
+
+        System.out.println("Panjabrao kalaskar");
+        
+
     }
 }
