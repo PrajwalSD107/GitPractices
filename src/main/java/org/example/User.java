edc7aa267975c95bd164c2a81db4ad1c2ec1835e
PrajwalSD107 <prajwalkalaskar107@gmail.com>
2026-06-21 15:40:05 +0530

    Add greetings for multiple users in User.java

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -3,5 +3,13 @@ package org.example;
 public class User {
     public static void main(String[] args) {
         System.out.println("Hello Prajwal!");
+
+         System.out.println("Hello Savita!");
+
+         System.out.println("Hello Panjabrao!");
+
+         System.out.println("Hello Pallavi!");
+
+        
     }
 }
